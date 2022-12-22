30de4ae9072d9afe36ff59195d1e1304d2d16de6
TranBach <TranBach@LAPTOP-ID8TN73Q>
2022-12-22 17:28:40 +0700

    date12-22-5h28

---
+++ DATN/Controllers/Mp4Controller.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace DATN.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Mp4Controller : ControllerBase
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public Mp4Controller(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetFileById()
+        {
+            string path = "C:/Users/TranBach/Downloads/mFWA.mp4";
+       
+
+            if (System.IO.File.Exists(path))
+            {
+                return  File(System.IO.File.OpenRead(path), "application/octet-stream", Path.GetFileName(path));
+            }
+            return NotFound();
+        }
+ 
+
+    }
+}
